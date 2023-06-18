a3c70c0b086bfb0fadf32bd258d196cc985f259a
Gaurav Singh <gauravkarki038@gmail.com>
2023-06-18 21:43:19 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,5 @@
+export const server = "https://eshop-backend-bga5.onrender.com/api/v2";
+
+export const backend_url = "https://eshop-backend-bga5.onrender.com/";
+
+export const ENDPOINT = "https://eshop-socket-xser.onrender.com/";
